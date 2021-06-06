8d97e50ffe7b230c19cf5c468d0d2d5f55931c86
Rohit singh <77694147+Rohit-singh19@users.noreply.github.com>
2021-06-06 19:56:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,31 @@
-const btn = document.getElementById('btn');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const pass = document.getElementById('pass');
-const vpass = document.getElementById('vpass');
+const fanBlades = document.querySelector('.fan-blades');
+const fanOnBtn = document.getElementById('fanon');
+const fastFan = document.getElementById('fanfast');
+const onBtn = document.getElementById('fanon');
+const fastBtn = document.getElementById('fanfast');
 
-let user = undefined;
-let em = undefined;
-let pas = undefined;
-let vpas = undefined;
+let x = undefined;
 
-btn.addEventListener('click', e => {
-    checkData();
-
-    if (user !== true || em !== true || pas !== true || vpass !== true) {
-        e.preventDefault();
+fanOnBtn.addEventListener('click', () => {
+    fanBlades.classList.remove('fast-animate')
+    fanBlades.classList.toggle('animate');
+    onBtn.classList.toggle('active');
+    fastBtn.classList.remove('active');
+    if (x == undefined || x == false) {
+        x = true;
+    }
+    else if (x == true) {
+        x = false;
     }
 });
 
-
-function checkData() {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = pass.value.trim();
-    const vpasswordValue = vpass.value.trim();
-
-
-    if (usernameValue === '') {
-        setError(username, 'Username cannot be blank');
-        user = false;
-    }else{
-        setNoError(username);
-        user = true;
+fastFan.addEventListener('click', (e) => {
+    if (x == true) {
+        fanBlades.classList.toggle('fast-animate');
+        fastBtn.classList.toggle('active');
     }
-
-    if (emailValue === '') {
-        setError(email, 'Email cannot be blank');
-        em = false;
-    }else if (!rEmail(emailValue)) {
-        setError(email, 'Email is not a valid');
-        em = false;
-    }else{
-        setNoError(email);
-        em = true;
+    else{
+        
+        e.preventDefault();
     }
-
-    if (passwordValue === '') {
-        setError(pass, 'Password cannot be blank');
-        pas = false;
-    }else{
-        setNoError(pass);
-        pas = true;
-    }
-
-    if (vpasswordValue === '') {
-        setError(vpass,'Password cannot be blank');
-        vpas = false;
-    } else if (passwordValue !== vpasswordValue) {
-        setError(vpass, 'Password does not match');
-        vpas = false;
-    }else{
-        setNoError(vpass);
-        vpas = true;
-    }
-
-}
-
-function setError(input, message) {
-    const formLabel = input.parentElement;
-    const small = formLabel.querySelector('small');
-    formLabel.className = 'form-label error';
-    small.innerText = message;
-}
-
-function setNoError(input) {
-    const formLabel = input.parentElement;
-    formLabel.className = 'form-label noerror';
-}
-
-function rEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-}
+});
